42714d065f089aad4daaeef51a84a34104e50230
Abhay Pratap Singh <145631801+ard3522o@users.noreply.github.com>
2025-05-24 11:15:05 +0530

    new changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const Listing = require("./models/listing.js");
 main().then(() => {
     console.log("connected to DB");
 }).catch((err) => {
@@ -12,6 +13,19 @@ async function main() {
 }
 app.get("/", (req, res)=> {
     res.send("Hi I am root");
+});
+app.get("/testListing", async (req, res) =>{
+let sampleListing = new Listing({
+    title: "My Home",
+    description: "By the beach",
+    price: 100,
+    location: "Azamgarh",
+    country: "India",
+
+});
+await sampleListing.save();
+console.log("sample was saved");
+res.send("successfull testing");
 });
 
 app.listen(8080, () => {
--- models/listing.js
+++ models/listing.js
@@ -8,11 +8,14 @@ const listingSchema = new Schema({
 
     },
     description: String,
-    image: String, 
+    image:{ 
+        type: String,
+    set: (v)=> v===""?"default link":"https://unsplash.com/photos/night-scene-of-a-busy-street-with-lights-u83VIV39t44",
+    }, 
     price: Number,
     location: String,
     country: String,
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
-modules.export = Listing;
+module.exports = Listing;
