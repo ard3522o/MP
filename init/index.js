b2af2333a15bfcdc7ef4d46627808b2ad3c438cd
Abhay Pratap Singh <145631801+ard3522o@users.noreply.github.com>
2025-05-25 12:35:14 +0530

    2

--- init/index.js
+++ init/index.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
-const listing = require("./models/listing.js");
+//const listing = require("../models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const Listing = require("./models/listing.js");
+const Listing = require("../models/listing.js");
 main().then(() => {
     console.log("connected to DB");
 }).catch((err) => {
@@ -14,8 +14,8 @@ async function main() {
 }
 
 const initDB = async() => {
-    await listing.deleteMany({});
-    await listing.insertMany(initData.data);
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
     console.log("data was initialised");
 
 }
