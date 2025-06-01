e5e9e0246c1a93b6f4531d8509359fbcdc27c3e1
Abhay Pratap Singh <145631801+ard3522o@users.noreply.github.com>
2025-06-01 11:11:18 +0530

    6

--- app.js
+++ app.js
@@ -22,12 +22,16 @@ app.get("/listings", async (req, res)=>{
   const allListings =  await Listing.find({});
   res.render("listings/index.ejs", {allListings});
     });
+    //new route
+app.get("/listings/new", (req, res) => {
+res.render("listings/new.ejs");
+});
+//show route
 app.get("/listings/:id", async(req, res)=>{
 let {id} = req.params;
  const listing = await Listing.findById(id);
  res.render("listings/show.ejs", {listing});
 });
-//show route
 
 // app.get("/testListing", async (req, res) =>{
 // let sampleListing = new Listing({
