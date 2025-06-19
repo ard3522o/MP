f0f2b8cef2f3f2f4aa5853e8fd63ea6f0b2ff451
Abhay Pratap Singh <145631801+ard3522o@users.noreply.github.com>
2025-06-19 10:35:30 +0530

    26

--- app.js
+++ app.js
@@ -84,6 +84,15 @@ app.post("/listings/:id/reviews", async (req, res) => {
    res.redirect(`/listings/${listing._id}`);
 });
 
+//Delete Review Route
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) =>{
+    let {id, reviewId} = req.params;
+ await   Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+await Review.findByIdAndDelete(reviewId);
+
+res.redirect(`/listings/${id}`);
+}));
+
 
 // app.get("/testListing", async (req, res) =>{
 // let sampleListing = new Listing({
